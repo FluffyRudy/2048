ddd3be2bcfd8025d5e520977dade1268fe010338
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-15 19:10:38 +0545

    complete basic game

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import readchar
 from enum import Enum, auto
 from os import system
-
+from random import randint, choice, choices
+from blockcolors import BLOCK_COLOR, RESET_COLOR
 
 class MovementType(Enum):
-    NULL_SHIFT = auto()
+    NULL_SHIFT = auto()  # if this occur in self.make_shift, it means there exist bug
     SHIFT_DONE = auto()
     NO_SLOT = auto()
 
 
 class Manager2048:
     def __init__(self):
-        self.grid = [[0, 0, 2, 2], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 2]]
+        self.grid = generate_init_grid()
         self.max_rows, self.max_cols = len(self.grid), len(self.grid[0])
         self.isover = False
         self.key_map_vertl = {
@@ -25,11 +26,20 @@ class Manager2048:
 
     def display_table(self):
         self.clear_window()
+
+        cell_width = 4
+        line_length = len(self.grid[0]) * (cell_width + 3) + 1
+        print(f"\033[1m{'_' * line_length}\033[0m")
+
         for row in self.grid:
-            line_length = len(row) * 3 + 2
-            print("_" * line_length)
-            print("|" + " | ".join(map(str, row)) + "|")
-        print("‾" * line_length)
+            print("| " + " | ".join(f"{"":^{cell_width}}" for _ in row) + " |")
+            row_str = "| " + " | ".join(
+                f"{BLOCK_COLOR.get(cell, RESET_COLOR)}{cell if cell != 0 else '':^{cell_width}}{RESET_COLOR}"
+                for cell in row
+            ) + " |"
+            print(row_str)
+
+        print(f"\033[1m{'‾' * line_length}\033[0m")
 
     def is_valid_position(self, row: int, col: int):
         return (
@@ -52,7 +62,6 @@ class Manager2048:
             start_row = self.max_rows - 1
             stop_row = -1
             step_row = -1
-        print(shift_x)
         if shift_x > 0:
             start_col = self.max_cols - 1
             stop_col = -1
@@ -117,6 +126,30 @@ class Manager2048:
                 self.handle_merging(shift_x, shift_y, new_row, new_col)
         return MovementType.SHIFT_DONE if blocks_moved else MovementType.NO_SLOT
 
+    def get_empty_pos(self) -> tuple[int, int]:
+        empty_positions = []
+        for rowindex, rowblock in enumerate(self.grid):
+            for colindex, block in enumerate(rowblock):
+                if block == 0:
+                    empty_positions.append((rowindex, colindex))
+        return [-1, -1] if len(empty_positions) == 0 else empty_positions
+
+    def generate_new_block(self) -> dict:
+        """keys: [row, col, block]"""
+        row, col = choice(self.get_empty_pos())
+        block = choices([2, 4, 8], weights=[80, 15, 5])
+        if row == -1 or col == -1:
+            return {}
+        return {"row": row, "col": col, "block": block[0]}
+
+    def assign_new_block(self) -> None:
+        new_block_info = self.generate_new_block()
+        if new_block_info:
+            row = new_block_info["row"]
+            col = new_block_info["col"]
+            block = new_block_info["block"]
+            self.grid[row][col] = block
+
     def handle_input(self):
         key = readchar.readkey()
         self.listen_for_exit(key)
@@ -127,7 +160,32 @@ class Manager2048:
 
             if shift_x or shift_y:
                 game_state = self.shift_blocks(shift_x, shift_y)
+                if game_state == MovementType.SHIFT_DONE:
+                    self.assign_new_block()
                 self.display_table()
+
+    def is_game_over(self):
+        positions = self.get_empty_pos()
+        if len(positions) == 2:
+            row, col = positions
+            return row == -1 and col == -1
+        return False
+
+    def replay(self):
+        try:
+            if input("play again:(y/n)").lower() == 'y':
+                self.grid = generate_init_grid()
+            else:
+                self.isover = True
+        except ValueError:
+            print("Invalid choice")
+
+    def has_won(self):
+        for blockrow in self.grid:
+            for block in blockrow:
+                if block == 2048:
+                    return True
+        return False
 
     def listen_for_exit(self, key: bytes):
         is_quitting = key == "q"
@@ -151,6 +209,31 @@ class Manager2048:
     def run(self):
         while not self.isover:
             self.handle_input()
+            if self.has_won():
+                print("You win")
+                self.replay()
+            elif self.is_game_over():
+                print("game over")
+                self.replay()
+                self.clear_window()
+                self.display_table()
+
+
+
+def generate_init_grid():
+    NROWS, NCOLS = 4, 4
+    grid = [[0 for _ in range(NCOLS)] for _ in range(NROWS)]
+
+    row1, col1 = randint(0, NROWS - 1), randint(0, NCOLS - 1)
+    row2, col2 = randint(0, NROWS - 1), randint(0, NCOLS - 1)
+
+    while (row1, col1) == (row2, col2):  # to ensure these two wont collide
+        row2, col2 = randint(0, NROWS - 1), randint(0, NCOLS - 1)
+
+    grid[row1][col1] = choice([2, 4, 8])
+    grid[row2][col2] = choice([2, 4, 8])
+
+    return grid
 
 
 def main():
