9655c59b1021928380d2197afbc83adc1990481a
Bishnu Chalise <cryptographycipher29@gmail.com>
2024-08-15 20:42:36 +0545

    add foreground and background color on  each cell

--- blockcolors.py
+++ blockcolors.py
@@ -1,17 +1,18 @@
 BLOCK_COLOR = {
-    0: "\033[0;30m",  # Black
-    2: "\033[1;37m",  # White
-    4: "\033[1;34m",  # Blue
-    8: "\033[1;33m",  # Yellow
-    16: "\033[1;32m",  # Green
-    32: "\033[1;35m",  # Magenta
-    64: "\033[1;36m",  # Cyan
-    128: "\033[1;31m",  # Red
-    256: "\033[0;31m",  # Dark Red
-    512: "\033[0;32m",  # Dark Green
-    1024: "\033[0;33m",  # Dark Yellow
-    2048: "\033[0;34m",  # Dark Blue
-    4096: "\033[1;37m",  # White
-    8192: "\033[1;37m",  # White
+    0: "\033[0;30m\033[40m",  # Black background, Black text
+    2: "\033[1;37m\033[47m",  # White text, White background
+    4: "\033[1;34m\033[44m",  # Blue text, Blue background
+    8: "\033[1;33m\033[43m",  # Yellow text, Yellow background
+    16: "\033[1;32m\033[42m",  # Green text, Green background
+    32: "\033[1;35m\033[45m",  # Magenta text, Magenta background
+    64: "\033[1;36m\033[46m",  # Cyan text, Cyan background
+    128: "\033[1;31m\033[41m",  # Red text, Red background
+    256: "\033[0;31m\033[41m",  # Dark Red text, Dark Red background
+    512: "\033[0;32m\033[42m",  # Dark Green text, Dark Green background
+    1024: "\033[0;33m\033[43m",  # Dark Yellow text, Dark Yellow background
+    2048: "\033[0;34m\033[44m",  # Dark Blue text, Dark Blue background
+    4096: "\033[1;37m\033[47m",  # White text, White background
+    8192: "\033[1;37m\033[47m",  # White text, White background
 }
+
 RESET_COLOR = "\033[0m"
